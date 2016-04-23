e66957411c73fd2ef2426728b9508d6aca1e8f5e
Artem Soroka <githubrepository@yandex.ru>
2016-04-23 15:17:11 +0000

    add function checkInvoiceStatus()

--- index.js
+++ index.js
@@ -22,18 +22,32 @@ Qiwi.prototype.client = function(config){
     return this; 
 }; 
 
-Qiwi.prototype._makeRequest = function(params){
+Qiwi.prototype._configRequest = function(params){
+    
     var headers = {
         'Accept'       : 'application/json', 
     	'Authorization': 'Basic ' + base64(this.APP_ID, this.API_PASS) 
     }; 
-    
-    extend(params, {headers: headers}); 
-    extend(params.form, {
+    var metadata = {
         lifetime: date(2, 'h'), 
         prv_name: this.SHOP_NAME 
+    }; 
+    
+    return new Promise(function(resolve, reject){
+        
+        extend(params, {headers: headers}); 
+        
+        if(params.method != 'GET'){
+            extend(params.form, metadata); 
+        }
+        
+        resolve(params); 
+        
     }); 
+}; 
 
+Qiwi.prototype._makeApiCall = function(params){
+    
     return new Promise(function(resolve, reject){
         request(params, function(err, response, data){
             if( err ){
@@ -48,13 +62,79 @@ Qiwi.prototype._makeRequest = function(params){
     
 }; 
 
-Qiwi.prototype.createInvoice = function(data){
-    
-    return this._makeRequest({
-        method: 'PUT', 
-        uri: [BASE_URL, 'prv', this.SHOP_ID, 'bills', data.id].join('/'), 
-        form: data
+Qiwi.prototype._parseApiResponse = function(response){
+    return new Promise(function(resolve, reject){
+        try{
+            var data = JSON.parse(response); 
+        } catch(e){
+            console.log(e); 
+            throw Error('Invalid API response: could not parse JSON string' + response); 
+        }
+        
+        if( !  data.response ){
+            throw Error('Invalid API response: "response" property is missing');         
+        } 
+            
+        resolve(data.response); 
     }); 
+}; 
+
+Qiwi.prototype._makeRequest = function(params){
+    var self = this; 
+    return this
+            ._configRequest(params)
+            .then(function(request){
+                return self._makeApiCall(request); 
+            })
+            .then(function(response){
+                return self._parseApiResponse(response); 
+            })
+            .catch(function(err){
+                console.log('error', err); 
+                throw err; 
+            }); 
+
+}; 
+
+Qiwi.prototype._parseApiError = function(response){
+    return (response.description) ? response.description : 'UNKNOWN ERROR'; 
+}; 
+
+Qiwi.prototype.createInvoice = function(data){
+    var self = this; 
+    return this
+            ._makeRequest({
+                method: 'PUT', 
+                uri: [BASE_URL, 'prv', this.SHOP_ID, 'bills', data.id].join('/'), 
+                form: data
+            })
+            .then(function(response){
+                if( response.result_code != 0){
+                    console.log('API responded with error code %d', response.result_code); 
+                    throw Error(self._parseApiError(response)); 
+                }
+                    
+                return response.bill; 
+            }); 
+    
+}; 
+
+Qiwi.prototype.checkInvoiceStatus = function(invoiceId){
+    var self = this; 
+    return this
+            ._makeRequest({
+                method: 'GET', 
+                uri: [BASE_URL, 'prv', this.SHOP_ID, 'bills', invoiceId].join('/') 
+            })
+            .then(function(response){
+                if( response.result_code != 0){
+                    console.log('API responded with error code %d', response.result_code); 
+                    throw Error(self._parseApiError(response)); 
+                }
+                
+                return response.bill; 
+                
+            }); 
     
 }; 
 
